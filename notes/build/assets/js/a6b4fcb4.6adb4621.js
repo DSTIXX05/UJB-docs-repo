5904713b709b165e3aa228f373264f52c72b23ab
Asolo <delightsomedunsiasolo24@gmail.com>
2026-05-25 21:37:43 +0100

    added build

---
+++ notes/build/assets/js/a6b4fcb4.6adb4621.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknotes=self.webpackChunknotes||[]).push([["8004"],{4019(e){e.exports=JSON.parse('{"metadata":{"permalink":"/previews/latest/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ notes/build/assets/js/acecf23e.999564a3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknotes=self.webpackChunknotes||[]).push([["3220"],{1912(e){e.exports=JSON.parse('{"blogBasePath":"/previews/latest/blog","blogTitle":"Blog","authorsListPath":"/previews/latest/blog/authors"}')}}]);
